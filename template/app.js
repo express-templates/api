c03f3511b44ec207bb225a331aae34750ae8ae06
NguyenThanh1995 <thanhnguyennguyen1995@gmail.com>
2021-05-26 19:43:58 +0700

    .

--- template/app.js
+++ template/app.js
@@ -54,13 +54,8 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  console.error(chalk.red(err.message));
+  res.end("Error");
 });
 
 const PORT = process.env.PORT || 3000;
--- template/app.ts
+++ template/app.ts
@@ -57,21 +57,15 @@ app.use(bodyParser.json());
 app.use(routes())
 
 // catch 404 and forward to error handler
-app.use((req: Request, res: Response, next: NextFunction): void => {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use((err: any, req: Request, res: Response): void => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.use((err, req, res) => {
+  console.error(chalk.red(err.message));
+  res.end("Error");
 });
-
 
 const PORT: number = +(process.env.PORT || 3000);
 
